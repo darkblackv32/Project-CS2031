3c4a7ef6abdf1bfbfca78126b3dff75001e0730d
Roger <rhuauyam@uni.pe>
2023-07-05 00:02:11 -0500

    updated env variables

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,3 @@
-// export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/bbuddy";
-export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000";
+export const API_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL;
 
 
