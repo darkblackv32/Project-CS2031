349d000bfe8d9e89150a49ff5ce20b83ad53e8d0
roger <rhuauyam@uni.pe>
2023-07-09 18:12:04 -0500

    removed links

--- server/config.js
+++ server/config.js
@@ -4,4 +4,3 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
